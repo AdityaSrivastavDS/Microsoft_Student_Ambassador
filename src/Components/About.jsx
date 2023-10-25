fb205948c27f88b3625da5e6a0fc820931c64587
ADITYA SRIVASTAV <114422969+AdityaSrivastavDS@users.noreply.github.com>
2023-10-25 11:38:34 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Data Science and Artificial Intelligence student studying at Babu Banarasi Das University. I enjoy enjoy the process of uncovering insights from data, applying machine learning techniques, and solving real-world problems using programming and statistical skills.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "Java",
+  "Data Analytics",
+  "Power BI",
+  "Machine Learning",
+  "Database",
 ];
 
 /**
@@ -45,8 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "I am passionate about leveraging data-driven approaches to solve complex problems and extract meaningful insights using a combination of programming, statistics, and machine learning techniques.";
 const About = () => {
   return (
     <section className="padding" id="about">
